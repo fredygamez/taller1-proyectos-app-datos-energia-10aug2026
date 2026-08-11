f440a3afbd33668e977a8895bdbddd0a0020c5bd
Fredy Gamez <fredygamez@gmail.com>
2026-08-10 20:08:35 -0500

    funcion load-data

--- app.py
+++ app.py
@@ -22,7 +22,10 @@ app.config.suppress_callback_exceptions = True
 # Load data from csv
 def load_data():
     # To do: Completar la función 
-    
+    df = pd.read_csv('datos_energia.csv')
+    df['fecha'] = pd.to_datetime(df['fecha'])
+    df = df.set_index('fecha')
+    return df
 
 # Cargar datos
 data = load_data()
